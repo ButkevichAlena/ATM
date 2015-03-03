0061b998dbdcacc34cc6f7c30ce7ae3ebf325b0b
ButkevichAlena <alena-butkevich@mail.ru>
2015-03-04 00:58:48 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMСonsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader();
+            reader.Read();
+            ATM atm = new ATM();
+            atm.Create(reader.Read());
+
+            atm.distributionOfBanknotes(500000);
+ 
+        }
+ 
+    }
+}
